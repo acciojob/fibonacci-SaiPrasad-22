cb2351fe51b9c0e075aaea8a803c7a41e7a355ea
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-03-26 19:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+
+	let arr = [0,1];
+	let n = arr.length;
+
+	for (let i=num; i<=n-1; i++){
+		arr[i] = arr[i-1] + arr[i-2];
+		return arr
+	}
 }
 
 module.exports = fibonacci;
