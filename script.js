3891a04e67e2f1fbc77fee6236f9032996a4b5b7
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-03-27 18:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function fibonacci(num) {
 // your code here
- if (num === 0) {
+ if (num === 1) {
         return 0;
-    } else if (num === 1) {
-        return 1;
     } else {
         // Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
         return fibonacci(num - 1) + fibonacci(num - 2);
