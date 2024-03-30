7a5064f4810b285be2ea710da496062b837500bb
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-03-30 18:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function fibonacci(num) {
-// your code here
- if (num === 1) {
-        return 0;
-    } else {
-        // Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
-        return fibonacci(num - 1) + fibonacci(num - 2);
-    }
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
 
-// module.exports = fibonacci;
+module.exports = fibonacci;
